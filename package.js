f8d66ed5d8991a01f7f87a15fa531fd1b8888508
Esilos92 <vendette@galaxycon.com>
2025-06-06 06:35:53 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 {
   "name": "flickr-photo-uploader",
   "version": "1.0.0",
+  "description": "Flickr photo uploader",
   "main": "api/index.js",
   "engines": {
     "node": "22.x"
@@ -10,5 +11,6 @@
   },
   "dependencies": {
     "flickr-sdk": "7.0.0-beta.9"
-  }
+  },
+  "license": "MIT"
 }
