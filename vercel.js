6ee595153f16068c8e59ade1fec87762c9e1d0f3
Esilos92 <vendette@galaxycon.com>
2025-06-06 06:16:02 -0400

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,14 +1,3 @@
 {
-  "version": 2,
-  "functions": {
-    "*.js": {
-      "runtime": "nodejs22.x"
-    }
-  },
-  "routes": [
-    {
-      "src": "/",
-      "dest": "/index.js"
-    }
-  ]
+  "version": 2
 }
