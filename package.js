0ebed81ce98147589f377193f8ff69891d10994c
Esilos92 <vendette@galaxycon.com>
2025-06-06 04:54:02 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,38 @@
+{
+  "name": "flickr-photo-uploader",
+  "version": "1.0.0",
+  "description": "Production-ready Flickr photo uploader with rate limiting, error handling, and privacy controls",
+  "main": "index.js",
+  "engines": {
+    "node": "18.x"
+  },
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Add tests here\" && exit 0",
+    "dev": "vercel dev",
+    "deploy": "vercel --prod"
+  },
+  "dependencies": {
+    "flickr-sdk": "^7.0.0-beta.9"
+  },
+  "keywords": [
+    "flickr",
+    "photo",
+    "upload",
+    "api",
+    "vercel",
+    "serverless",
+    "privacy",
+    "dropbox"
+  ],
+  "author": "Your Name",
+  "license": "MIT",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/yourusername/flickr-uploader.git"
+  },
+  "bugs": {
+    "url": "https://github.com/yourusername/flickr-uploader/issues"
+  },
+  "homepage": "https://github.com/yourusername/flickr-uploader#readme"
+}
